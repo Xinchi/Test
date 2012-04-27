fbb639eb966accedafed87733bd0103f222faed7
Gu Xinchi <guxinchi2000@gmail.com>
2012-04-27 21:39:18 +0800

    Initial Commit

---
+++ TestRepositoryProjectTests/TestRepositoryProjectTests.h
@@ -0,0 +1,13 @@
+//
+//  TestRepositoryProjectTests.h
+//  TestRepositoryProjectTests
+//
+//  Created by Max Gu on 4/27/12.
+//  Copyright (c) 2012 guxinchi2000@gmail.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestRepositoryProjectTests : SenTestCase
+
+@end
---
+++ TestRepositoryProjectTests/TestRepositoryProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  TestRepositoryProjectTests.m
+//  TestRepositoryProjectTests
+//
+//  Created by Max Gu on 4/27/12.
+//  Copyright (c) 2012 guxinchi2000@gmail.com. All rights reserved.
+//
+
+#import "TestRepositoryProjectTests.h"
+
+@implementation TestRepositoryProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestRepositoryProjectTests");
+}
+
+@end
